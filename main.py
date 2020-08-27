ca26116d29531add9c5eb083a0e15e9dd97a352e
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-08-27 19:41:20 +0200

    typos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 class public_key:
     def __init__(self, n, e):
         self.n = n
@@ -16,7 +16,7 @@ def GCD(a, b):
     else:
         return GCD(b, a%b)
 
-def invmod(a,b): return 0 if a==0 else 1 if b%a==0 else b - invmod(b%a,a)*b//a
+def inverseMod(x, y): return 0 if x==0 else 1 if y%x==0 else y - inverseMod(y%x ,x)*y//x
 
 def modExp(a, e, m):
     a = a % m
@@ -25,28 +25,19 @@ def modExp(a, e, m):
     if e & 1: return a * modExp(a, e - 1, m) % m
     else: return modExp(a * a, e >> 1, m) % m
 
-def mulmod(a, b, m):
-    x = 0
-    y = a % m
-    while b > 0:
-        if b % 2 == 1:
-            x = (x + y) % m
-        y = (y * 2) % m
-        b //= 2
-    return x % m
-
 def isPrime(n) :   
     if (n <= 1) : 
         return False
     if (n <= 3) : 
         return True
-    if (n % 2 == 0 or n % 3 == 0) : 
+    if (n % 2 == 0 or n % 3 == 0): 
         return False
-    i = 5
-    while(i * i <= n) : 
-        if (n % i == 0 or n % (i + 2) == 0) : 
-            return False
-        i = i + 6
+    for i in range(4,int(math.sqrt(n))):
+        k = i
+        while(k * k <= n) : 
+            if (n % k == 0) : 
+                return False
+            k = k + i
     return True
 
 def generate_keys():
@@ -56,14 +47,14 @@ def generate_keys():
     e = 0
     phi = 0
     while True:
-        p = random.randint(2**30, 2**40)
-        q = random.randint(2**30, 2**40)
+        p = random.randint(2**10, 2**20)
+        q = random.randint(2**10, 2**20)
         n = p * q
         phi = (p - 1) * (q - 1)
-        e = random.randint(2, 2**30) % phi 
+        e = random.randint(2, 2**20) % phi 
         if p != q and GCD(phi, e) == 1 and isPrime(p) and isPrime(q):
             break
-    d = invmod(e, phi)
+    d = inverseMod(e, phi)
     pub = public_key(n, e)
     priv = private_key(n, d)
     return pub, priv
